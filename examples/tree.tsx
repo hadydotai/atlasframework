1cb61bc4f5bb84cfc5eb789371feedcbe48ef4b3
Hady <204673822+hadydotai@users.noreply.github.com>
2026-09-21 13:39:18 +0200

    introduce an indirection (nested function component inside <message> tag) in the example

--- examples/tree.tsx
+++ examples/tree.tsx
@@ -4,6 +4,10 @@ interface ReviewerProps {
 	code: string;
 }
 
+function Code({ code } : { code: string }) {
+	return ["Source code:\n", code];
+}
+
 function Reviewer({ code }: ReviewerProps) {
 	console.log("Reviewer executed");
 	return (
@@ -11,7 +15,19 @@ function Reviewer({ code }: ReviewerProps) {
 			<message role="system">
 				Review the code for reptitive patterns.
 			</message>
-			<message role="user">{code}</message>
+			<message role="user">
+				{/* This is a contrived example but tests whether we can traverse
+					nested function components inside <message> tags
+					that return whatever is acceptable to <message> tags. In our
+					current implementation thats strings, arrays of strings, and
+					numbers. Though we don't specifically call out arrays of
+					non-string values. Actually, thinking about it now as I type this,
+					no need because if it's stringifable then we should be okay.
+					We'll likely behave like react too in that regard, need to give it
+					more thought
+				*/}
+				<Code code={code} />
+			</message>
 		</agent>
 	);
 }
